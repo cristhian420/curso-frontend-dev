eaac9781ff7eac715f2644fec4f420fa22d5957d
Cristhian Salas <cristhian.sm91@gmail.com>
2022-08-27 19:03:47 -0500

    Se agrega Js a menu mobile y menu desktop

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const emailUser = document.querySelector('.nav-right-email');
 const menuEmail = document.querySelector('.menu-nav-main-container');
+const closeIconMobile = document.querySelector('.icono');
+const menuMobileImg = document.querySelector('.menu-nav-right');
+const menuMobile = document.querySelector('.menu-mobile--total-container');
 
-emailUser.addEventListener('click', tooglemenuEmail);
 
+emailUser.addEventListener('click', toogleMenuEmail);
+closeIconMobile.addEventListener('click',toogleMenuMobile);
+menuMobileImg.addEventListener('click',toogleMenuMobile);
 
-function tooglemenuEmail(){
+function toogleMenuEmail(){
     menuEmail.classList.toggle('inactive');
+}
+
+function toogleMenuMobile(){
+    menuMobile.classList.toggle('inactive');
 }
