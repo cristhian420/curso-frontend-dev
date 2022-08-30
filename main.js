a1be2c40611b9fe38c8d311485eecd029b73dbcb
Cristhian Salas <cristhian.sm91@gmail.com>
2022-08-30 14:24:51 -0500

    Se agrega lista de productos dinamicos y se maqueta en js para crear elementos en HTML

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const menuMobileImg = document.querySelector('.menu-nav-left--icon');
 const menuMobile = document.querySelector('.menu-mobile--total-container');
 const cartIcon = document.querySelector('.nav-right--cartIcon');
 const myOrderMenu = document.querySelector('.aside--my-order-cart');
-
+const cardsContainer = document.querySelector('.cards-container');
 
 emailUser.addEventListener('click', toogleMenuEmail);
 closeIconMobile.addEventListener('click',toogleMenuMobile);
@@ -42,4 +42,65 @@ function toogleCartMenu(){
 
 }
 
-console.log()
+function renderProducts(arr){
+    for(product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+        productImg.classList.add('product-img');
+        
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+        
+        const productInfoText = document.createElement('div');
+        
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoText.append(productName, productPrice);
+
+        const productInfoFigure = document.createElement('figure');
+        const productCartIcon = document.createElement('img');
+        productCartIcon.setAttribute('src', '/icons/bt_add_to_cart.svg');
+        
+        productInfoFigure.append(productCartIcon);
+        productInfo.append(productInfoText,productInfoFigure);
+        productCard.append(productImg,productInfo);
+        
+        
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+const productList = [{
+    name: 'playera',
+    price: 120,
+    image: 'https://cdn.shopify.com/s/files/1/0365/1921/5241/products/20201003_131156_530x@2x.jpg?v=1601754362'
+},
+{
+    name: 'blusa',
+    price: 100,
+    image: 'https://m.media-amazon.com/images/I/71jJW0yEygS._AC_UX385_.jpg'
+},
+{
+    name: 'falda',
+    price: 80,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdx7g45j8ZmMp9wUuZMpZ61hsOiVcGE34hyw&usqp=CAU'
+},
+{
+    name: 'gorra',
+    price: 105,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRutn0eeTMYlBdo3ClViFRAmM9SSuQtmoRZ4Q&usqp=CAU'
+},
+{
+    name: 'tenis',
+    price: 300,
+    image: 'https://http2.mlstatic.com/D_NQ_NP_658019-MLM44413340941_122020-O.webp'
+},];
+
+renderProducts(productList);
+
