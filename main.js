96db807937a1a388b0f5d2c91baf08b53a7f3d0a
Cristhian Salas <cristhian.sm91@gmail.com>
2022-09-01 18:43:46 -0500

    v1.0 version terminada del curso practico JS

--- main.js
+++ main.js
@@ -6,11 +6,48 @@ const menuMobile = document.querySelector('.menu-mobile--total-container');
 const cartIcon = document.querySelector('.nav-right--cartIcon');
 const myOrderMenu = document.querySelector('.aside--my-order-cart');
 const cardsContainer = document.querySelector('.cards-container');
+const closeIconDetail = document.querySelector('.product-detail--close');
+const productDetailCardImg = document.querySelector('.product-detail--img');
+const textContainerProductDetailCard = document.querySelector('.text-container--product-detail-card');
+const asideProductDetailContainer = document.querySelector('#aside--product-detail-card');
+const productList = [{
+    name: 'playera',
+    price: 120,
+    image: 'https://cdn.shopify.com/s/files/1/0365/1921/5241/products/20201003_131156_530x@2x.jpg?v=1601754362',
+    description: 'playera super way hippiosa'
+},
+{
+    name: 'blusa',
+    price: 100,
+    image: 'https://m.media-amazon.com/images/I/71jJW0yEygS._AC_UX385_.jpg',
+    description: 'blusa jippiosa whitexican'
+
+},
+{
+    name: 'falda',
+    price: 80,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdx7g45j8ZmMp9wUuZMpZ61hsOiVcGE34hyw&usqp=CAU',
+    description: 'falda super mamadora'
+},
+{
+    name: 'gorra',
+    price: 105,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRutn0eeTMYlBdo3ClViFRAmM9SSuQtmoRZ4Q&usqp=CAU',
+    description: 'gorra estilo 60s'
+},
+{
+    name: 'tenis',
+    price: 300,
+    image: 'https://http2.mlstatic.com/D_NQ_NP_658019-MLM44413340941_122020-O.webp',
+    description: 'tenis tipo fila pero hippies'
+},];
 
 emailUser.addEventListener('click', toogleMenuEmail);
 closeIconMobile.addEventListener('click',toogleMenuMobile);
 menuMobileImg.addEventListener('click',toogleMenuMobile);
 cartIcon.addEventListener('click',toogleCartMenu);
+closeIconDetail.addEventListener('click', toggleProductCardDetail)
+
 
 function toogleMenuEmail(){
     const isCartMenuOpen = !myOrderMenu.classList.contains('inactive');
@@ -33,13 +70,37 @@ function toogleMenuMobile(){
 function toogleCartMenu(){
     const isMenuMobileOpen = !menuMobile.classList.contains('inactive');
     const isMenuEmailOpen = !menuEmail.classList.contains('inactive');
+    const isProductDetailAsideOpen = !asideProductDetailContainer.classList.contains('inactive');
 
-    if(isMenuMobileOpen || isMenuEmailOpen) {
+    if(isMenuMobileOpen || isMenuEmailOpen || isProductDetailAsideOpen) {
         menuMobile.classList.add('inactive');
         menuEmail.classList.add('inactive');
+        asideProductDetailContainer.classList.add('inactive');
     }
     myOrderMenu.classList.toggle('inactive');
 
+}
+
+function toggleProductCardDetail(){
+    const isMenuMobileOpen = !menuMobile.classList.contains('inactive');
+    const isCartMenuOpen = !myOrderMenu.classList.contains('inactive');
+    const isMenuEmailOpen = !menuEmail.classList.contains('inactive');
+
+
+    if(isMenuMobileOpen || isMenuEmailOpen || isCartMenuOpen) {
+        menuMobile.classList.add('inactive');
+        menuEmail.classList.add('inactive');
+        myOrderMenu.classList.add('inactive');
+    }
+    asideProductDetailContainer.classList.toggle('inactive');
+
+}
+
+function closeAllMenus(){
+    menuMobile.classList.add('inactive');
+    menuEmail.classList.add('inactive');
+    asideProductDetailContainer.classList.add('inactive');
+    myOrderMenu.classList.add('inactive');
 }
 
 function renderProducts(arr){
@@ -50,6 +111,7 @@ function renderProducts(arr){
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
         productImg.classList.add('product-img');
+        productImg.addEventListener('click',toggleProductCardDetail);
         
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -76,31 +138,25 @@ function renderProducts(arr){
     }
 }
 
-const productList = [{
-    name: 'playera',
-    price: 120,
-    image: 'https://cdn.shopify.com/s/files/1/0365/1921/5241/products/20201003_131156_530x@2x.jpg?v=1601754362'
-},
-{
-    name: 'blusa',
-    price: 100,
-    image: 'https://m.media-amazon.com/images/I/71jJW0yEygS._AC_UX385_.jpg'
-},
-{
-    name: 'falda',
-    price: 80,
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdx7g45j8ZmMp9wUuZMpZ61hsOiVcGE34hyw&usqp=CAU'
-},
-{
-    name: 'gorra',
-    price: 105,
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRutn0eeTMYlBdo3ClViFRAmM9SSuQtmoRZ4Q&usqp=CAU'
-},
-{
-    name: 'tenis',
-    price: 300,
-    image: 'https://http2.mlstatic.com/D_NQ_NP_658019-MLM44413340941_122020-O.webp'
-},];
+
+    // productDetailCardImg.setAttribute('src', productList.image);
+    // const textContainerPrice = document.createElement('p');
+    // textContainerPrice.classList.add('precio');
+    // textContainerPrice.innerText = '$' + productList.price;
+    
+    // const textContainerTitle = document.createElement('p');
+    // textContainerTitle.classList.add('titulo');
+    // textContainerTitle.innerText = productList.name;
+    
+    // const textContainerDescription = document.createElement('p');
+    // textContainerDescription.classList.add('description');
+    // textContainerDescription.innerText = productList.description;
+
+    // textContainerProductDetailCard.append(textContainerPrice, textContainerTitle, textContainerDescription);
+
+
+
 
 renderProducts(productList);
+
 
